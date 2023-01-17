2a8723503ea37d4ca7c02012fbb066121741b403
farzadsnj <farzadsanjarani3@gmail.com>
2023-01-17 15:32:27 +0330

    co1

---
+++ src/components/DataFetching.js
@@ -0,0 +1,38 @@
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+
+function DataFetching() {
+    const[posts, setPosts] = useState({})
+    const[id, setId] = useState(1)
+    const[IdFromButton, setIdFromButton] = useState(1)
+
+    const handleClick = ()=>{
+        setIdFromButton(id)
+    }
+
+    useEffect(()=>{
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${IdFromButton}`)
+        .then(res =>{
+            console.log(res)
+            setPosts(res.data)
+        })
+        .catch(err =>{
+            console.log(err)
+        })
+    },[IdFromButton])
+  return (
+    <div>
+        <input type='text' value={id} onChange={e =>setId(e.target.value)} />
+        <button onClick={handleClick} type='button'>Fetch Data</button>
+        <div>{posts.title}</div>
+        
+        {/* <ol>
+            {
+                posts.map(post => <li key={post.id}>{post.title}</li>)
+            }
+        </ol> */}
+    </div>
+  )
+}
+
+export default DataFetching
---
+++ src/components/HookTimer.js
@@ -0,0 +1,22 @@
+import React, {useState, useEffect, useRef} from 'react'
+
+function HookTimer() {
+    const [timer, setTimer] = useState(0)
+    const intervalRef = useRef()
+
+    useEffect(()=>{
+        intervalRef.current = setInterval(()=>{
+            setTimer(prevState => prevState + 1)
+        }, 1000)
+        return() =>{
+            clearInterval(intervalRef.current)
+        }
+    }, [])
+  return (
+    <div>HookTimer - {timer}
+        <button onClick={()=> clearInterval(intervalRef.current)}>Clear</button>
+    </div>
+  )
+}
+
+export default HookTimer
